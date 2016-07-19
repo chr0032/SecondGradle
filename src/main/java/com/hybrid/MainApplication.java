1f25b859f4a58a7f8c2be630e76b71ebef697712
java <java>
2016-07-19 18:44:27 +0900

    second commit

---
+++ src/main/java/com/hybrid/MainApplication.java
@@ -0,0 +1,14 @@
+package com.hybrid;
+							  
+public class MainApplication {
+   							 
+public static void main(String[] args) {
+      						  
+System.out.println("##############");		  
+System.out.println("Hello SecondGradle");
+System.out.println("##############");
+System.out.println("##############");
+System.out.println("##############");
+  							  
+}							  
+}
